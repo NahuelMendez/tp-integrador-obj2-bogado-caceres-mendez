6f0593b7082037a1d280c09b33c8cb5e69cfdd41
ximena-bogado <ximenakbq@gmail.com>
2020-06-04 17:51:51 -0300

    agrego funcionalidad externa

---
+++ src/ar/edu/unq/TPIntegrador/FuncionalidadExterna.java
@@ -0,0 +1,7 @@
+package ar.edu.unq.TPIntegrador;
+
+public interface FuncionalidadExterna {
+
+	void nuevoEvento(Organizacion organizacionSalud, ZonaDeCobertura zonaDeCobertura, Muestra muestra);
+
+}
