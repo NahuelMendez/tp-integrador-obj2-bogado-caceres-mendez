566d97834c4f7b3953cbd598fda268352260374c
ximena-bogado <ximenakbq@gmail.com>
2020-07-01 19:14:41 -0300

    comentarios borrados

--- src/ar/edu/unq/TPIntegrador/IObserver.java
+++ src/ar/edu/unq/TPIntegrador/IObserver.java
@@ -3,8 +3,6 @@ package ar.edu.unq.TPIntegrador;
 import ar.edu.unq.TPIntegrador.MuestraYEstados.Muestra;
 
 public interface IObserver {
-	
-	//public void actualizar(ZonaDeCobertura zonaDeCobertura, Muestra muestra, String aviso);
 
 	public void actualizarNuevaMuestra(ZonaDeCobertura zonaDeCobertura, Muestra muestra);
 
